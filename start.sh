53c21cf73918d7abb4cd5b720d0495bc342d6f5c
Santiago Agudelo <sagudelo1200@gmail.com>
2021-04-17 01:41:01 -0500

    export of environment variables needed for the database

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+export QUIMBAYA_MYSQL_HOST='localhost'
+export QUIMBAYA_MYSQL_USER='agudelo'
+export QUIMBAYA_MYSQL_PWD='PROTECTED-DATA'
+export QUIMBAYA_MYSQL_DB='quimbaya'
+export QUIMBAYA_TYPE_STORAGE='db'
+# export QUIMBAYA_ENV='test' # Warning: if the value is 'test', all data in the db will be deleted
 uvicorn api.v1.main:app --host 0.0.0.0 --port 5000 --reload --header server:agudelo
